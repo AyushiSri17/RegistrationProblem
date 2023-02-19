b2017cef81ec4265778caed165a4b0ad9085c0c6
“AyushiSri17” <srivastavaayushi7110@gmail.com>
2023-02-19 20:53:11 +0530

    [Ayushi] Added Validation for mobile number

--- RegistrationProblem/Program.cs
+++ RegistrationProblem/Program.cs
@@ -12,7 +12,7 @@ namespace RegistrationProblem
         {
             Console.WriteLine("Welcome to Registration Program");
             Console.WriteLine("Choose the option for validation you want to perform");
-            Console.WriteLine("1. Validate First Name \n2. Validate Last Name \n3. Validate Email");
+            Console.WriteLine("1. Validate First Name \n2. Validate Last Name \n3. Validate Email \n4. Validate Mobile Number");
             int option=Convert.ToInt32(Console.ReadLine()); 
             switch(option)
             {
@@ -24,6 +24,9 @@ namespace RegistrationProblem
                     break;
                 case 3:
                     UserValidation.ValidateEmail();
+                    break;
+                case 4:
+                    UserValidation.ValidateMobileNumber();
                     break;
                 default:
                     Console.WriteLine("Please Choose the correct option");
